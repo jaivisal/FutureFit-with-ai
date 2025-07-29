9d14dafdc02066dfc3b0948b72fe970c38035752
jaivisal <138554017+jaivisal@users.noreply.github.com>
2025-07-29 22:02:35 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made by JAIV </p>
+                <p>Made by JAIV AND GURU </p>
               </div>
             </footer>
           </ThemeProvider>
